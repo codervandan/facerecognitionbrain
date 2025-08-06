1d9ff5bc87d6c3bddc1dec1fd7e7075a5bde8e10
codervandan <danielquintanadev@protonmail.com>
2025-08-06 14:08:51 -0700

    new changes adding button cursor

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -5,9 +5,15 @@ import './Logo.css'
 
 const Logo = () => {
   return (
-      <Tilt className="Tilt br2 shadow-2 w3" tiltMaxAngleX={55}>
-        <div className="Tilt Tilt-inner">
-          <img src={brain}/>
+      <Tilt 
+        className="Tilt br2 shadow-2 w3" tiltMaxAngleX={55}
+        style={{paddingTop: '5px'}}
+      >
+        <div className="Tilt Tilt-inner pa3">
+          <img 
+            src={brain}
+            // style={{paddingTop: '5px'}}
+          />
         </div>
       </Tilt>
   )
